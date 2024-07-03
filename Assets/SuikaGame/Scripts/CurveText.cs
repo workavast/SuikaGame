8a90457062f82422d9eddf3a0118c19768a891e5
Rodion <workavast@yandex.ru>
2024-07-04 00:56:18 +0300

    basic integration of sprites in ui

---
+++ Assets/SuikaGame/Scripts/CurveText.cs
@@ -0,0 +1,119 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace SuikaGame.Scripts
+{
+    public class CurveText : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text tmpText;
+        [SerializeField] private AnimationCurve vertexCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.25f, 2.0f), new Keyframe(0.5f, 0), new Keyframe(0.75f, 2.0f), new Keyframe(1, 0f));
+        [SerializeField] private float curveScale = 1.0f;
+        
+        void Start()
+        {
+            ApplyCurve();
+        }
+        
+        private void OnValidate()
+        {
+            if(tmpText == null)
+                return;
+            
+            ApplyCurve();
+        }
+        
+        [ContextMenu("Apply Curve")]
+        private void ApplyCurve()
+        {
+            if (tmpText == null)
+            {
+                Debug.LogWarning($"tmpText is null");
+                return;
+            }
+            
+            vertexCurve.preWrapMode = WrapMode.Clamp;
+            vertexCurve.postWrapMode = WrapMode.Clamp;
+
+            //Mesh mesh = m_TextComponent.textInfo.meshInfo[0].mesh;
+            
+            tmpText.havePropertiesChanged = true; // Need to force the TextMeshPro Object to be updated.
+            
+            if (!tmpText.havePropertiesChanged)
+                return;
+            
+            tmpText.ForceMeshUpdate(); // Generate the mesh and populate the textInfo with data we can use and manipulate.
+
+            TMP_TextInfo textInfo = tmpText.textInfo;
+            if(textInfo == null)
+                return;
+            int characterCount = textInfo.characterCount;
+            
+            if (characterCount == 0)
+                return;
+
+            //vertices = textInfo.meshInfo[0].vertices;
+            //int lastVertexIndex = textInfo.characterInfo[characterCount - 1].vertexIndex;
+
+            float boundsMinX = tmpText.bounds.min.x; //textInfo.meshInfo[0].mesh.bounds.min.x;
+            float boundsMaxX = tmpText.bounds.max.x; //textInfo.meshInfo[0].mesh.bounds.max.x;
+
+
+            for (int i = 0; i < characterCount; i++)
+            {
+                if (!textInfo.characterInfo[i].isVisible)
+                    continue;
+
+                int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+
+                // Get the index of the mesh used by this character.
+                int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+
+                var vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                // Compute the baseline mid point for each character
+                Vector3 offsetToMidBaseline =
+                    new Vector2((vertices[vertexIndex + 0].x + vertices[vertexIndex + 2].x) / 2,
+                        textInfo.characterInfo[i].baseLine);
+                //float offsetY = VertexCurve.Evaluate((float)i / characterCount + loopCount / 50f); // Random.Range(-0.25f, 0.25f);
+
+                // Apply offset to adjust our pivot point.
+                vertices[vertexIndex + 0] += -offsetToMidBaseline;
+                vertices[vertexIndex + 1] += -offsetToMidBaseline;
+                vertices[vertexIndex + 2] += -offsetToMidBaseline;
+                vertices[vertexIndex + 3] += -offsetToMidBaseline;
+
+                // Compute the angle of rotation for each character based on the animation curve
+                float x0 = (offsetToMidBaseline.x - boundsMinX) /
+                           (boundsMaxX - boundsMinX); // Character's position relative to the bounds of the mesh.
+                float x1 = x0 + 0.0001f;
+                float y0 = vertexCurve.Evaluate(x0) * curveScale;
+                float y1 = vertexCurve.Evaluate(x1) * curveScale;
+
+                Vector3 horizontal = new Vector3(1, 0, 0);
+                //Vector3 normal = new Vector3(-(y1 - y0), (x1 * (boundsMaxX - boundsMinX) + boundsMinX) - offsetToMidBaseline.x, 0);
+                Vector3 tangent = new Vector3(x1 * (boundsMaxX - boundsMinX) + boundsMinX, y1) -
+                                  new Vector3(offsetToMidBaseline.x, y0);
+
+                float dot = Mathf.Acos(Vector3.Dot(horizontal, tangent.normalized)) * 57.2957795f;
+                Vector3 cross = Vector3.Cross(horizontal, tangent);
+                float angle = cross.z > 0 ? dot : 360 - dot;
+
+                var matrix = Matrix4x4.TRS(new Vector3(0, y0, 0), Quaternion.Euler(0, 0, angle), Vector3.one);
+
+                vertices[vertexIndex + 0] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 0]);
+                vertices[vertexIndex + 1] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 1]);
+                vertices[vertexIndex + 2] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 2]);
+                vertices[vertexIndex + 3] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 3]);
+
+                vertices[vertexIndex + 0] += offsetToMidBaseline;
+                vertices[vertexIndex + 1] += offsetToMidBaseline;
+                vertices[vertexIndex + 2] += offsetToMidBaseline;
+                vertices[vertexIndex + 3] += offsetToMidBaseline;
+            }
+            
+            // Upload the mesh with the revised information
+            tmpText.UpdateVertexData();
+        }
+    }
+}
--- Assets/SuikaGame/Scripts/Score/IScoreCounter.cs
+++ Assets/SuikaGame/Scripts/Score/IScoreCounter.cs
@@ -5,11 +5,16 @@ namespace SuikaGame.Scripts.Score
     public interface IScoreCounter
     {
         public int Score { get; }
+        public int Record { get; }
 
         /// <summary>
         /// return current score value
         /// </summary>
         public event Action<int> OnScoreChanged;
+        /// <summary>
+        /// return current record value
+        /// </summary>
+        public event Action<int> OnRecordChanged;
 
         public void Reset();
     }
