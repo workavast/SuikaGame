72054c2349a8a3957b473a944dd90764aa17116e
Rodion <workavast@yandex.ru>
2024-07-04 11:16:45 +0300

    import GamePush

---
+++ Assets/Plugins/GamePush/Runtime/Modules/GP_Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+using GamePush.ConsoleController;
+
+namespace GamePush
+{
+    public class GP_Server : MonoBehaviour
+    {
+        [DllImport("__Internal")]
+        private static extern string GP_ServerTime();
+
+        public static DateTime Time()
+        {
+#if !UNITY_EDITOR && UNITY_WEBGL
+            return DateTime.Parse(GP_ServerTime(), System.Globalization.CultureInfo.InvariantCulture);
+#else
+            if (GP_ConsoleController.Instance.ServerConsoleLogs)
+                Console.Log("SERVER: ", "TIME: " + DateTime.Now);
+            return DateTime.Now;
+#endif
+        }
+
+    }
+}
