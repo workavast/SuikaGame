69117140096d18876ec2a9d81d10222d7c272163
Rodion <workavast@yandex.ru>
2024-07-13 22:40:26 +0300

    create camera size adapter, to guarantee that game field will be in the camera view

---
+++ Assets/SuikaGame/Scripts/CameraSizeAdapting/CameraSizeAdapter.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+namespace SuikaGame.Scripts.CameraSizeAdapting
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraSizeAdapter : MonoBehaviour
+    {
+        [SerializeField] private Vector2Int referenceResolution;
+
+        private Camera _camera;
+        private float _prevAspect;
+        private float _refAspect;
+        private float _defaultSize;
+        private Vector3 _defaultPosition;
+        
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            _refAspect = (float)referenceResolution.y / referenceResolution.x;
+            _defaultSize = _camera.orthographicSize;
+            _defaultPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            var curAspect = (float)Screen.height / Screen.width;
+
+            if(Math.Abs(curAspect - _prevAspect) < 0.0001f)
+                return;
+            
+            Debug.Log("SDASD");
+            
+            if (_refAspect < curAspect)
+            {
+                var newSize = _defaultSize * curAspect / _refAspect;
+                _camera.orthographicSize = newSize;
+
+                var newPosition = _defaultPosition + Vector3.up * (newSize - _defaultSize);
+                transform.position = newPosition;
+            }
+            else
+            {
+                _camera.orthographicSize = _defaultSize;
+                transform.position = _defaultPosition;
+            }
+
+            _prevAspect = curAspect;
+        }
+    }
+}
