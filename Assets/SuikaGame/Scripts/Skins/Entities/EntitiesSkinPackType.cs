ce240591dc28a026a7861bb36402bcc5aac97804
Rodion <workavast@yandex.ru>
2024-08-06 16:52:20 +0300

    create new skin packs

--- Assets/SuikaGame/Scripts/Skins/Entities/EntitiesSkinPackType.cs
+++ Assets/SuikaGame/Scripts/Skins/Entities/EntitiesSkinPackType.cs
@@ -4,5 +4,10 @@ namespace SuikaGame.Scripts.Skins.Entities
     {
         Fruits = 0,
         Cats = 10,
+        FruitsVector = 20,
+        ExoticFruitsVector = 30,
+        VegetablesVector = 40,
+        IceCream = 50,
+        RuCoins = 60
     }
 }
