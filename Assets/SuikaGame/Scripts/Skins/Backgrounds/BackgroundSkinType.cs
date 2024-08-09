2feb0425076a3c669ab6b8b2e1e60da3fbec256a
Rodion <workavast@yandex.ru>
2024-08-09 20:00:52 +0300

    add new background skins

--- Assets/SuikaGame/Scripts/Skins/Backgrounds/BackgroundSkinType.cs
+++ Assets/SuikaGame/Scripts/Skins/Backgrounds/BackgroundSkinType.cs
@@ -4,5 +4,15 @@ namespace SuikaGame.Scripts.Skins.Backgrounds
     {
         Fruits = 0,
         Cats = 10,
+        Dogs = 15,
+        FruitsVector = 20,
+        ExoticFruitsVector = 30,
+        VegetablesVector = 40,
+        IceCream = 50,
+        RuCoins = 60,
+        GravityFalls = 80,
+        Pokemons = 90,
+        HayaoMiyazaki = 100,
+        AdventureTime = 110
     }
 }
